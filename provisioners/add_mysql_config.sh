c4f8301329e186fff435907e5073f99f37471853
carlosdgerez <carlosgerez@byui.edu>
2025-02-07 19:37:52 +0100

    add documentation in add-mysql_config.sh

--- provisioners/add_mysql_config.sh
+++ provisioners/add_mysql_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# This script will run the first time vagrant file is call.
 # Ensure the MySQL configuration file exists
 if [ -f /etc/mysql/my.cnf ]; then
   echo -e "\n[mysqld]\nbind-address = 0.0.0.0" |  tee -a /etc/mysql/my.cnf
